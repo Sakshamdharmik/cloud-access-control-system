5dbf6c82f047d070ac7746fbfee0f82213c68a96
Sakshamdharmik <sakshamdharmik@gmail.com>
2025-04-08 19:51:47 +0530

    Cleaned up .pyc and venv files, updated .gitignore

--- db_config.py
+++ db_config.py
@@ -1,10 +1,15 @@
-# db_config.py
+import mysql.connector
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()  # Loads .env file variables into environment
 
-DB_HOST = os.getenv("DB_HOST")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
+def get_db_connection():
+    connection = mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        port=3306,
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
+    )
+    return connection
